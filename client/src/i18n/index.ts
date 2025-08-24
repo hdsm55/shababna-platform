b8f70c61aaa0a42c355f21e10769911aa166bdaf
Husam Al-Dhaheri <hossamaldahry@gmail.com>
2025-08-24 12:50:22 +0300

    Comment out LanguageDetector usage in i18n configuration to optimize initial bundle size.

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -1,13 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpBackend from 'i18next-http-backend';
 
 // Load translations on-demand via HTTP to reduce initial bundle size
 
 i18n
   .use(HttpBackend)
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'ar',
